6c593823bccb2cd32906684a967046a495a16ca0
Sholem <sholem@Elazar-Rivkins-MacBook-Pro.local>
2015-05-29 14:11:13 -0400

    equation 1 works

---
+++ project_euler.py
@@ -0,0 +1,41 @@
+'''
+1. List all the natural numbers from 0 to 10 that are mulitples of 3 and 5
+
+2. List all natural numbers from 1 to 1000 that are mulitple of 3 and 5 and the return the sum
+'''
+
+
+def if_multiple(nat_number):
+    ''' return True if it is a multiple of 3 or 5'''
+    
+    if nat_number % 3 == 0 or nat_number  % 5 == 0:
+        
+        print nat_number
+      
+        return True
+    
+    else :
+    
+        return False
+
+'''end of one funcetion '''
+
+
+
+
+
+def append_to_array(myarray, item_to_append):
+    '''appends item_to_array to myarray '''
+
+
+
+
+
+
+
+def sum_numbers( number ):
+    ''' Sum all of the nmbers'''
+    
+    return
+
+
